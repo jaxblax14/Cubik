9d3f7e7fca774241dc19ef79bfba65be3ab40c69
cristianahue666 <cristian.ahumadae9@gmail.com>
2019-02-27 17:39:16 -0300

    introduccion de las unidades

--- app/controllers/challenge_intro_controller.rb
+++ app/controllers/challenge_intro_controller.rb
@@ -2,6 +2,7 @@ class ChallengeIntroController < ApplicationController
   before_action(:set_challenge, only: [:intro])
   def intro
     @units = Unit.all
+    @challenge_has_units = ChallengeHasUnit.all
   end
 
 
