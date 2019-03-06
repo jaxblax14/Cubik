e6a1cb4636eacdab19cde17b0853ba1c5a93ce38
dinocarlos <eldinocarlos@gmail.com>
2019-03-06 17:01:30 -0300

    flix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_194645) do
+ActiveRecord::Schema.define(version: 2019_03_06_195111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,10 +136,10 @@ ActiveRecord::Schema.define(version: 2019_03_05_194645) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.datetime "date_of_birth"
-    t.string "provider"
-    t.string "uid"
     t.text "image"
     t.boolean "suscription"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
