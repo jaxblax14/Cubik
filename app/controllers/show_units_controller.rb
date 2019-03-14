d3959ba311d8b1fd0049ed294184b4481565efef
jaxblax14 <pablohalo112@hotmail.com>
2019-03-14 13:13:57 -0300

    subida cambios vistas

--- app/controllers/show_units_controller.rb
+++ app/controllers/show_units_controller.rb
@@ -55,7 +55,7 @@ class ShowUnitsController < ApplicationController
   end
   def finalizar_desafio
      @project.project_state_id = 3
-     
+     @project.boards.update(project_id: nil, users_id: nil)
     if @project.save
       flash[:notice] = "Tu projecto a finalizado correctamente"
       redirect_to(dashboard_index_path)
